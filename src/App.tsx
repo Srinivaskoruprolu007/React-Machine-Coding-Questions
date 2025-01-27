7170a84f567abe8789aa43f866e3556f3fcd6243
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-01-27 17:57:47 +0530

    updated with accordian component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import Counter from "./components/Easy/Counter";
+import Accordion from "./components/Easy/Accordion";
 
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "Why use React?",
+    content: "React makes it painless to create interactive UIs",
+  },
+];
 const App = () => {
   return (
     <div>
       <Counter initialCount={10} />
+      <Accordion items={items} />
     </div>
   );
 };
